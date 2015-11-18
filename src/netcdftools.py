cfcc91d0e51608ede2647d2753b352ba0f202df9
Raphael Cobe <raphaelmcobe@gmail.com>
2015-11-18 16:57:36 -0200

    Updated README API description

---
+++ src/netcdftools.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from netCDF4 import Dataset
+
+def toJSON(start_date_time, end_date_time, variable):
+    """
+    This function receives a time frame and produces a json version of the data
+    stored at the netcdf repository.
+    """
+    pass 
+
